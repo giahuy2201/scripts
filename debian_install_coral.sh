3a8a00e28c7f8f8682dfb81edc001ebd1d5abe41
Huy Nguyen <giahuy2201@icloud.com>
2024-04-03 13:45:19 -0400

    Fix "public key not available"

--- debian_install_coral.sh
+++ debian_install_coral.sh
@@ -1,6 +1,5 @@
 mkdir -p -m 755 /etc/apt/keyrings &&
-    curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | tee /etc/apt/keyrings/google-cloud.gpg &&
-    chmod a+r /etc/apt/keyrings/google-cloud.gpg &&
+    wget -qO - https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/google-cloud.gpg &&
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-cloud.gpg] https://packages.cloud.google.com/apt coral-edgetpu-stable main" | tee /etc/apt/sources.list.d/coral-edgetpu.list &&
     apt update &&
     apt install -y libedgetpu1-std gasket-dkms
