3607e4ab7626ddfa56b8fc87c353194c9296cf62
Huy Nguyen <giahuy2201@icloud.com>
2024-04-03 13:50:07 -0400

    Fix gasket-dkms build error by installing from source

--- debian_install_coral.sh
+++ debian_install_coral.sh
@@ -2,4 +2,10 @@ mkdir -p -m 755 /etc/apt/keyrings &&
     wget -qO - https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/google-cloud.gpg &&
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-cloud.gpg] https://packages.cloud.google.com/apt coral-edgetpu-stable main" | tee /etc/apt/sources.list.d/coral-edgetpu.list &&
     apt update &&
-    apt install -y libedgetpu1-std gasket-dkms
+    apt install -y libedgetpu1-std git devscripts dh-dkms &&
+    git clone https://github.com/google/gasket-driver.git $HOME/gasket-driver &&
+    cd $HOME/gasket-driver &&
+    debuild -us -uc -tc -b &&
+    cd .. &&
+    dpkg -i gasket-dkms_*.deb &&
+    apt update && apt upgrade
